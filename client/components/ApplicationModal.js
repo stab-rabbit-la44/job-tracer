2c629a744aba0dc5e6218fef982197b1462448e4
Vivian Wu <vivian.wu.here@gmail.com>
2021-07-10 18:47:09 -0700

    frontend setup with react

---
+++ client/components/ApplicationModal.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// import Modal from 'react-modal';
+
+export default function ApplicationModal() {
+  return <div>I am the ApplicationModal</div>;
+}
---
+++ client/reducers/index.js
@@ -0,0 +1,5 @@
+// import { combineReducers } from 'redux'
+
+// import reducers from './reducers';
+
+// export combineReducers w/ obj of key applications: reducers
