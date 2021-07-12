304a71e92caa69428f1067ecf87aacd1d5791b9f
Vivian Wu <vivian.wu.here@gmail.com>
2021-07-12 12:48:15 -0700

    update App.js and Modal - modal has basic form fields

--- client/App.js
+++ client/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 // import * as actions from './actions/actions';
 import JobApplicationList from './containers/JobApplicationList';
+import CreateJobApplicationModal from './components/modals/CreateJobApplicationModal';
 
 const mapStateToProps = (state) => ({
   apps: state.applications.applications,
@@ -11,11 +12,14 @@ const mapStateToProps = (state) => ({
 // const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch);
 
 const App = (props) => {
+  const [show, setShow] = useState(false);
+
   return (
     <div>
-      <button type="button" onClick={() => console.log('create button here')}>
+      <button type="button" onClick={() => setShow(true)}>
         Create
       </button>
+      <CreateJobApplicationModal onClose={() => setShow(false)} show={show} />
       <JobApplicationList jobApps={props.apps} />
     </div>
   );
